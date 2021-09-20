893cd90c9b3fbaa1b6da1cb95187acca2cb01c91
Arnau Tresserras <arnau.tresserras@itequia.com>
2021-09-20 16:46:20 +0200

    Swagger character and enum

---
+++ Models/RpgClass.cs
@@ -0,0 +1,12 @@
+using System.Text.Json.Serialization;
+
+namespace dotnet_rpg.Models
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum RpgClass
+    {
+        Knight,
+        Mage,
+        Cleric
+    }
+}
