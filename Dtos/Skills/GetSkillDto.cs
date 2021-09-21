d9e0c6dbb42e4891f164f56e0619f7749541da0c
Arnau Tresserras <arnau.tresserras@itequia.com>
2021-09-21 22:15:46 +0200

    Skill data and Dto

---
+++ Dtos/Skills/GetSkillDto.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_rpg.Dtos.Skills
+{
+    public class GetSkillDto
+    {
+        public string Name { get; set; }
+        public int Damage { get; set; }
+    }
+}
